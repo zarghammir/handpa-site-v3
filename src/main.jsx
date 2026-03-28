0dd8c8c43b8258e1970cc9b2e92622b5a97657da
zargham mirzaei <zargham@zarghams-MacBook-Pro.local>
2026-03-27 18:42:23 -0700

    fix: ReactDOM import and HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { HelmetProvider } from "react-helmet-async";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { HelmetProvider } from "react-helmet-async"
+import App from "./App"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>,
-);
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+)
